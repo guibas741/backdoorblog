94480179c06d8508cb464e32c05d289658aefd88
Guibas <bublitz.stein@gmail.com>
2018-05-12 14:26:15 -0300

    Add submenu functionality to titles' page

--- src/js/titles.js
+++ src/js/titles.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    $('#verticalMenu').menu();
+    subMenu();
 });
+
+
+function subMenuId(id) {
+    $(id).click(function() {
+        $(id + ' ul').toggle();
+    });
+}
+
+function subMenu() {
+    subMenuId('#year2013');
+    subMenuId('#year2014');
+    subMenuId('#year2015');
+    subMenuId('#year2016');
+    subMenuId('#year2017');
+    subMenuId('#year2018');
+}
