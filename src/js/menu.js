f4c44a9d9c8d51f4ee22a92d2cc8308b698343c7
Guibas <bublitz.stein@gmail.com>
2018-05-17 00:16:02 -0300

    Change members menu to positions menu and add member menu as a submenu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+    menuPositions();
+    menuPlayers('top','toplist');
+    menuPlayers('jungle','junglelist');
+    menuPlayers('mid','midlist');
+    menuPlayers('adc','adclist');
+    menuPlayers('support','supportlist');
+});
 
+function menuPositions() {
     $('#players').click(function() {
         $('.header.submenu').slideToggle('medium');
         return false;
-    })
-});
+    });
+}
+
+function menuPlayers(id, classe) {
+    $('#' +id).mouseover(function() {
+        $('.'+classe).show('medium');
+    }).mouseleave(function() {
+        $('.'+classe).hide('medium');
+    });
+    return false;
+}
 
